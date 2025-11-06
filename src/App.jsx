206a564d5b0d4ce6418655a0e8982ec331fe6ad0
Powza72 <manu.aon212@gmail.com>
2025-11-06 18:04:27 +0700

    fix: correct Navbar import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Players from './pages/Players'
 import Home from './pages/Home'
 import Team from './pages/Team'
